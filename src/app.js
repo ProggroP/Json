f7b305ea932166f2c277fba40589d03e5b4938e2
phntxx <meissner.bastian@gmail.com>
2016-04-20 21:01:46 +0200

    Version Alpha 0.2
    -Added App Icon

--- src/app.js
+++ src/app.js
@@ -7,10 +7,35 @@ var weatherkey = '87c21b8d0b617b8ac9240ff198fb6395';
 var weathername = 'Hirschau';
 var weatherurl = 'http://api.openweathermap.org/data/2.5/weather?q=' + weathername + '&appid=' + weatherkey;
 
+//Variables for getting the bitcoin Value
+var bitcoincur = 'EUR';
+var bitcoinurl = 'http://api.bitcoinaverage.com/exchanges/' + bitcoincur;
+
+//Variables for getting twitter trends
+var twittertrendsurl = 'http://api.whatthetrend.com/api/v2/trends.json';
+
 //Weather Menu
 var weathercard = new UI.Card({
-  title:'Weather',
-  subtitle:'Fetching...'
+   backgroundcolor: '#7f8c8d',
+   textColor: '#d35400',
+   title:'Weather',
+   subtitle:'Fetching...'
+});
+
+//Bitcoin Menu
+var bitcoincard = new UI.Card({
+   backgroundcolor: '#7f8c8d',
+   textColor: '#d35400',
+   title:'Bitcoin Value',
+   subtitle:'Fetching...'
+});
+
+//Twitter Trend Menu
+var twittertrendscard = new UI.Card({
+   backgroundcolor: '#7f8c8d',
+   textColor: '#d35400',
+   title:'Twitter Trends',
+   subtitle:'Fetching...'
 });
 
 //Main Menu
@@ -24,9 +49,11 @@ var menu = new UI.Menu({
          title: 'Weather',
          icon: 'images/menu_icon.png'
       }, {
-         title: 'Second Item',
-         icon: 'images/menu_icon.png',
-         subtitle: 'Subtitle Text'
+         title: 'Bitcoin Value',
+         icon: 'images/menu_icon.png'
+      }, {
+         title: 'Trending Tweets',
+         icon: 'images/menu_icon.png'
       }]
    }]
 });
@@ -35,6 +62,10 @@ var menu = new UI.Menu({
 menu.on('select', function(e){
    if (e.itemIndex === 0){
       weathercard.show();
+   } else if (e.itemIndex == 1){
+      bitcoincard.show();
+   } else if (e.itemIndex == 2){
+      twittertrendscard.show();
    }
 });
 
@@ -53,6 +84,30 @@ function(data) {
 function(error) {
    console.log('Failed fetching weather data: ' + error);
 });
+
+//Code for BitCoin Menu
+ajax(
+   {url: bitcoinurl, type: 'json'},
+   function(data){
+      console.log("Successfully fetched bitcoin data!");
+      var ask = data.bitcoin_de.rates.ask;
+      var name = data.bitcoin_de.display_name;
+      bitcoincard.subtitle("The current value on " + name + " is " + ask + " EUR.");
+   }
+);
+
+ajax(
+   {url: twittertrendsurl, type: 'json'},
+   function(data){
+      console.log("Successfully fetched trend data!");
+      var date = data.trends.first_trended_at;
+      var name = data.trends.desctription.text;
+      console.log(date);
+      console.log(name);
+   }
+);
+
+
 
 menu.show();
 
